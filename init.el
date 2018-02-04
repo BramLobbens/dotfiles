56feef58f798c77594a8f2499fb4ea6c90d4f0d1
Bram Lobbens <lobbensbram@gmail.com>
2018-02-04 18:49:45 +0100

    add magit package

--- init.el
+++ init.el
@@ -10,13 +10,13 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar myPackages
+(defvar myPackages ;; M-x package-refresh-contents RET after adding a package
   '(ein
     elpy
     flycheck
-    material-theme
     dracula-theme
     py-autopep8
+    magit
     ))
 
 (mapc #'(lambda (package)
@@ -35,8 +35,7 @@
       `((".*" ,temporary-file-directory t)))
 
 (setq inhibit-startup-message t) ;; hide the startup message
-;(load-theme 'material t) ;; load material theme
-(load-theme 'dracula t)
+(load-theme 'dracula t) ;; load theme
 (global-linum-mode t) ;; enable line numbers globally
 (set-face-attribute 'default nil :font "Monaco-12") ;; set default font
 (menu-bar-mode -1) ;; hide menu bar
@@ -44,12 +43,15 @@
   (tool-bar-mode -1))
 (toggle-scroll-bar -1) ;; disable scroll bar
 
+;; CUSTOM KEYBINDS
+;; --------------------------------------
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
 (elpy-enable)
-;(elpy-use-ipython)
-;(setq ansi-color-for-comint-mode t)
 
 ;; use flycheck instead of flymake with elpy
 (when (require 'flycheck nil t)
@@ -64,35 +66,7 @@
 ;; C CONFIGURATION
 ;; --------------------------------------
 
-(defun c-lineup-arglist-tabs-only (ignored)
-    "Line up argument lists by tabs, not spaces"
-    (let* ((anchor (c-langelem-pos c-syntactic-element))
-           (column (c-langelem-2nd-pos c-syntactic-element))
-           (offset (- (1+ column) anchor))
-           (steps (floor offset c-basic-offset)))
-      (* (max steps 1)
-         c-basic-offset)))
-
-  (add-hook 'c-mode-common-hook
-            (lambda ()
-              ;; Add kernel style
-              (c-add-style
-               "linux-tabs-only"
-               '("linux" (c-offsets-alist
-                          (arglist-cont-nonempty
-                           c-lineup-gcc-asm-reg
-                           c-lineup-arglist-tabs-only))))))
-
-  (add-hook 'c-mode-hook
-            (lambda ()
-              (let ((filename (buffer-file-name)))
-                ;; Enable kernel mode for the appropriate files
-                (when (and filename
-                           (string-match (expand-file-name "~/src/linux-trees")
-                                         filename))
-                  (setq indent-tabs-mode t)
-                  (setq show-trailing-whitespace t)
-                  (c-set-style "linux-tabs-only")))))
-
+(setq c-default-style "linux"
+          c-basic-offset 4)
 
 ;; init.el ends here
