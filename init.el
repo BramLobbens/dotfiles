b0f8f2b5fd874475ddb14372c625e2ceb01a4738
Bram Lobbens <lobbensbram@gmail.com>
2018-08-11 16:36:59 +0200

    update: init.el

--- init.el
+++ init.el
@@ -18,6 +18,14 @@
     py-autopep8
     magit
     haskell-mode
+    multiple-cursors
+    neotree
+    all-the-icons
+    telephone-line
+    anaconda-mode
+    racket-mode
+    rainbow-delimiters
+    paredit
     ))
 
 (mapc #'(lambda (package)
@@ -39,11 +47,12 @@
 (load-theme 'dracula t) ;; load theme
 (global-linum-mode t) ;; enable line numbers globally
 (setq column-number-mode t) ;; display column number
-(set-face-attribute 'default nil :font "Monaco-12") ;; set default font
+(set-face-attribute 'default nil :font "Monaco-14") ;; set default font
 (menu-bar-mode -1) ;; hide menu bar
 (when (fboundp 'tool-bar-mode) ;; hide tool bar
   (tool-bar-mode -1))
 (toggle-scroll-bar -1) ;; disable scroll bar
+(global-prettify-symbols-mode +1) ;; globally use prettify-symbols
 
 ;; CUSTOM KEYBINDS
 ;; --------------------------------------
@@ -64,6 +73,8 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
 ;; C CONFIGURATION
 ;; --------------------------------------
@@ -71,5 +82,20 @@
 (setq c-default-style "linux"
           c-basic-offset 4)
 
+;; RACKET CONFIGURATION
+;; --------------------------------------
+
+(require 'rainbow-delimiters-mode)
+(add-hook 'racket-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'racket-mode-hook           #'enable-paredit-mode)
+
+;; OTHER CONFIGS
+;; --------------------------------------
+(require 'neotree)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(global-set-key [f8] 'neotree-toggle)
+
+(require 'telephone-line)
+(telephone-line-mode 1)
 ;; init.el ends here
 
